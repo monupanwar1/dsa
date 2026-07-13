f339dcbf5ec8bb377716a7963c7a9dda0b18478a
monupanwar1 <monupanwar5655@gmail.com>
2026-07-13 19:03:41 +0530

    count prime no

---
+++ countprime.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+
+  if (n <= 1)
+    return false;
+
+  for (int i = 2; i * i <= n; i++)
+  {
+    if (n % i == 0)
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+int countprime(int n)
+{
+
+  int count = 0;
+
+  for (int i = 2; i < n; i++)
+  {
+
+    if (isPrime(i))
+    {
+      count++;
+    }
+  }
+  return count;
+}
+
+int main()
+{
+  int n = 10;
+  cout << countprime(n);
+  return 0;
+}
