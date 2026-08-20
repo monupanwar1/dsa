b7ae465d66680a91543135f453b44a0b8d15e4b0
monupanwar1 <monupanwar5655@gmail.com>
2026-08-20 20:53:04 +0530

    2Sum

---
+++ array/2Sum.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> Sum(vector<int> &arr, int target)
+{
+  int n = arr.size();
+
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = i + 1; j < n; j++)
+    {
+
+      if (arr[i] + arr[j] == target)
+      {
+        return {i, j};
+      }
+    }
+  }
+  return {};
+}
+bool Sum2(vector<int> &arr, int target)
+{
+  int n = arr.size();
+
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = i + 1; j < n; j++)
+    {
+
+      if (arr[i] + arr[j] == target)
+      {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+int main()
+
+{
+  vector<int> arr = {4, 7, 1, 9, 3, 6, 2};
+  int target = 10;
+
+  vector<int> ans = Sum(arr, target);
+
+  for (auto it : ans)
+  {
+    cout << it << " ";
+  }
+
+  cout << endl;
+
+  cout << Sum2(arr, target);
+}
