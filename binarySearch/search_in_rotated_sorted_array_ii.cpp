68251d8e04b5b2b415aa6a25117f01dfce534172
monupanwar1 <monupanwar5655@gmail.com>
2026-09-05 20:58:02 +0530

    search_in_rotated_sorted_array_ii

---
+++ binarySearch/search_in_rotated_sorted_array_ii.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool search(vector<int> &arr, int target)
+{
+  int low = 0;
+  int high = arr.size() - 1;
+
+  while (low <= high)
+  {
+
+    int mid = low + (high - low) / 2;
+
+    if (arr[mid] == target)
+    {
+      return true;
+    }
+
+    if (arr[mid] == arr[low] && arr[mid] == arr[high])
+    {
+      low++;
+      high--;
+    }
+    else if (arr[low] <= arr[mid])
+    {
+      if (arr[low] <= target && target <= arr[mid])
+      {
+        high = mid - 1;
+      }
+      else
+      {
+        low = mid + 1;
+      }
+    }
+    else
+    {
+      if (arr[mid] <= target && target <= arr[high])
+      {
+        low = mid + 1;
+      }
+      else
+      {
+        high = mid - 1;
+      }
+    }
+  }
+
+  return false;
+}
+
+int main()
+{
+  vector<int> arr = {2, 5, 6, 0, 0, 1, 2};
+  int target = 0;
+
+  cout << search(arr, target);
+
+  return 0;
+}
